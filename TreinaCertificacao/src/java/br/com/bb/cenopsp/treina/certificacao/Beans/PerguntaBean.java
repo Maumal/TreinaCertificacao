4bac685defcae2dea5b345ee01bdb13a71931768
f7057419 <f7057419@esspo031981d37.sp.intrabb.bb.com.br>
2015-03-27 15:42:35 -0300

    Inicio do Projeto - Testando passagem de valor para página

---
+++ TreinaCertificacao/src/java/br/com/bb/cenopsp/treina/certificacao/Beans/PerguntaBean.java
@@ -0,0 +1,92 @@
+
+package br.com.bb.cenopsp.treina.certificacao.Beans;
+
+
+import br.com.bb.cenopsp.treina.certificacao.entidade.Pergunta;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+//@ViewScoped
+@SessionScoped
+public class PerguntaBean  implements Serializable {    
+  private int numeroDeQuestoes; 
+  private Pergunta pergunta ;
+  private List<Pergunta> listaPerguntas;
+ 
+ public void PerguntaBean() { 
+        pergunta=new Pergunta();
+        listaPerguntas=new ArrayList<>();
+        listaPerguntas.add(new Pergunta("Agronegócios", "2009_1","12", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Atendimento Bancário", "2009_1","23", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Comércio Exterior", "2009_1","25", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Conhecimentos Jurídicos", "2009_1","2", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Controles Internos e Risco Operacional", "2009_1","5", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("DRS/RSA", "2009_1","4", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Gestão do Crédito", "2009_1","24", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Gestão Organizacional e de Pessoas", "2009_1","12", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Gestão da Segurança", "2009_1","47", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Marketing", "2009_1","20", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Setor Público", "2009_1","14", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Tecnologia da Informação", "2009_1","12", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Economia e Finanças", "2009_1","18", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        
+        listaPerguntas.add(new Pergunta("Agronegócios", "2009_2","25", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Atendimento Bancário", "2009_2","4", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Comércio Exterior", "2009_2","5", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Conhecimentos Jurídicos", "2009_2","13", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Controles Internos e Risco Operacional","12", "2009_2", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("DRS/RSA", "2009_2","16", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Gestão do Crédito", "2009_2","17", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Gestão Organizacional e de Pessoas", "2009_2","7", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Gestão da Segurança", "2009_2","1", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Marketing", "2009_2","2", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Setor Público", "2009_2","16", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Tecnologia da Informação", "2009_2","1", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));
+        listaPerguntas.add(new Pergunta("Economia e Finanças", "2009_2","18", "P 1", "A1", "A2", "A3", "A4", "A5", "C",null));   
+        this.numeroDeQuestoes=listaPerguntas.size();
+    }
+    
+ 
+ 
+ public void inserirPergunta(){
+        listaPerguntas.add(pergunta);
+        pergunta=new Pergunta();
+    }
+
+    public int getNumeroDeQuestoes() {
+        return numeroDeQuestoes;
+    }
+
+   
+
+    public Pergunta getPergunta() {
+        return pergunta;
+    }
+
+    public void setPergunta(Pergunta pergunta) {
+        this.pergunta = pergunta;
+    }
+
+    public List<Pergunta> getListaPerguntas() {
+        return listaPerguntas;
+    }
+
+    public void setListaPerguntas(List<Pergunta> listaPerguntas) {
+        this.listaPerguntas = listaPerguntas;
+    }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    
+}
---
+++ TreinaCertificacao/src/java/br/com/bb/cenopsp/treina/certificacao/entidade/Pergunta.java
@@ -0,0 +1,131 @@
+package br.com.bb.cenopsp.treina.certificacao.entidade;
+
+import java.util.ArrayList;
+
+public class Pergunta {
+    private String marteria;
+    private String ano;
+    private String NumeroQuestao;
+    private String enunciado;
+    private String alternativaA;
+    private String alternativaB;
+    private String alternativaC;
+    private String alternativaD;
+    private String alternativaE;
+    private String Resposta;
+    private  ArrayList<String> comentarios;
+
+    public Pergunta() {
+    }
+
+    public Pergunta(String marteria, String ano, String NumeroQuestao, String enunciado, String alternativaA, String alternativaB, String alternativaC, String alternativaD, String alternativaE, String Resposta, ArrayList<String> comentarios) {
+        this.marteria = marteria;
+        this.ano = ano;
+        this.NumeroQuestao = NumeroQuestao;
+        this.enunciado = enunciado;
+        this.alternativaA = alternativaA;
+        this.alternativaB = alternativaB;
+        this.alternativaC = alternativaC;
+        this.alternativaD = alternativaD;
+        this.alternativaE = alternativaE;
+        this.Resposta = Resposta;
+        this.comentarios = comentarios;
+    }
+
+    public ArrayList<String> getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(ArrayList<String> comentarios) {
+        this.comentarios = comentarios;
+    }
+
+
+    
+    
+
+    public String getNumeroQuestao() {
+        return NumeroQuestao;
+    }
+
+    public void setNumeroQuestao(String NumeroQuestao) {
+        this.NumeroQuestao = NumeroQuestao;
+    }
+
+    
+    public String getMarteria() {
+        return marteria;
+    }
+
+    public void setMarteria(String marteria) {
+        this.marteria = marteria;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getEnunciado() {
+        return enunciado;
+    }
+
+    public void setEnunciado(String enunciado) {
+        this.enunciado = enunciado;
+    }
+
+    public String getAlternativaA() {
+        return alternativaA;
+    }
+
+    public void setAlternativaA(String alternativaA) {
+        this.alternativaA = alternativaA;
+    }
+
+    public String getAlternativaB() {
+        return alternativaB;
+    }
+
+    public void setAlternativaB(String alternativaB) {
+        this.alternativaB = alternativaB;
+    }
+
+    public String getAlternativaC() {
+        return alternativaC;
+    }
+
+    public void setAlternativaC(String alternativaC) {
+        this.alternativaC = alternativaC;
+    }
+
+    public String getAlternativaD() {
+        return alternativaD;
+    }
+
+    public void setAlternativaD(String alternativaD) {
+        this.alternativaD = alternativaD;
+    }
+
+    public String getAlternativaE() {
+        return alternativaE;
+    }
+
+    public void setAlternativaE(String alternativaE) {
+        this.alternativaE = alternativaE;
+    }
+
+    public String getResposta() {
+        return Resposta;
+    }
+
+    public void setResposta(String Resposta) {
+        this.Resposta = Resposta;
+    }
+  
+    
+    
+    
+}
